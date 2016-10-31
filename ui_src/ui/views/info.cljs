18ee148211c06e7725e0e001b25b5c003142c228
Dmitry Kolobov <dmkolobov@gmail.com>
2016-10-31 14:31:40 -0600

    Update info rendering.

--- ui_src/ui/views/info.cljs
+++ ui_src/ui/views/info.cljs
@@ -1,7 +1,9 @@
 (ns ui.views.info
-  (:require [re-com.core :refer [md-icon-button v-box h-box box title]]
+  (:require [re-com.core :refer [md-icon-button v-box h-box box title label]]
             [re-frame.core :refer [dispatch]]
             [cljs.pprint :refer [pprint]]))
+
+(defonce file-path (js/require "path"))
 
 (defn info-button
   []
@@ -11,18 +13,32 @@
                   :tooltip-position :below-left
                   :on-click         #(dispatch [:images/open-info])])
 
+(defn human-filesize
+  [filesize]
+  (let [[num scale] (cond
+                      (< filesize (* 1000 1000))      [(/ filesize 1000) "KB"]
+                      (< filesize (* 1000 1000 1000)) [(/ filesize 1000000) "MB"]
+                      :default                        [(/ filesize 1000000000) "GB"])]
+    (str (.toFixed num 2) " " scale)))
+
+(defn human-size
+  [{:keys [width height]}]
+  (str width " x " height))
+
 (defn display-info
   [model]
-  (let [[[path info] _ _]  @model
-        printed-info       (with-out-str (pprint info))]
+  (let [[[path {:keys [filesize taken-at size]}] _ _]  @model]
     ^{:key path}
     [v-box :class    "fullscreen-sidebar"
            :height   "100%"
            :size     "none"
            :padding  "0 1em"
            :children [[h-box :align :center
-                       :children [[box :size "auto" :child [title :level :level2 :label "Info"]]
-                                  [md-icon-button :md-icon-name "zmdi-close"
-                                   :size         :regular
-                                   :on-click     #(dispatch [:images/close-info])]]]
-                      [box :child [:pre printed-info]]]]))
+                             :children [[box :size "auto" :child [title :level :level2 :label "Info"]]
+                                        [md-icon-button :md-icon-name "zmdi-close"
+                                         :size         :regular
+                                         :on-click     #(dispatch [:images/close-info])]]]
+                                        [title :label (.basename file-path path) :level :level3]
+                                        [h-box :gap      "1em"
+                                               :children [[label :label (human-size size)]
+                                                          [label :label (human-filesize filesize)]]]]]))
